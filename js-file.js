e5319e6be3b4371b25afc6a0b64bd95874c5ec0b
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-12 00:39:33 -0700

    Attempt to change the color of each div
    As the mouse passes through, the color of each div should change based
    on the mouse entering a new div. I think the problem might be that the
    divs I created can't be access from outside the for loop. Maybe need to
    find a way to access them from outside the loop?

--- js-file.js
+++ js-file.js
@@ -1,11 +1,13 @@
-
-
-
-
-for (i=0; i <= 16; i++) {
-    const container = document.querySelector('div');
+for (i=0; i <= 255; i++) {
+    const container = document.querySelector('.container');
     container.style.display = 'flex'
+    container.style.flexWrap = 'wrap'
+    container.style.maxWidth = '690px'
     let divGrid = document.createElement('div');
     divGrid.style.cssText = 'height: 40px; width: 40px; border: 1px solid black;' ;
     container.appendChild(divGrid); 
 }
+
+addEventListener('onmouseenter', () => {
+    div.style.color = 'green'
+})
