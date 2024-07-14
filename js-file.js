aac4d432010d49f2333dc92161e96f35f33cbfa5
Dylan DeFrank <defrankdylan@gmail.com>
2024-07-14 14:38:14 -0700

    Create a button that generates a new grid
    I added an event listener to the button that would take the prompt and
    have that number be the amount of times that the for loop iterated. A
    prompt of 40 would produce 40 squares.
    
    The real trouble was getting the grid to reset itself before taking a
    new prompt. The initial behavior would be the squares would just add on
    to what was already there. I fixed this by adding another event listener
    before the one that produces squares.
    
    This event listener would check if there was an element on the page
    that was made by the other event listener. If true, it would run a while
    loop that removed all of it's children.

--- js-file.js
+++ js-file.js
@@ -1,5 +1,27 @@
-for (i=0; i <= 255; i++) {
+const userInput = document.querySelector('button')
+
+userInput.addEventListener('click', () => {
+    let check = document.getElementById('check')
+    if (check) {
+        while (check.firstChild) {
+            check.removeChild(check.firstChild)
+        }
+    } else return {
+
+}})
+
+userInput.addEventListener('click', () => {
+   
+    let gridSize = prompt('What grid size would you like?')
+    getGridSize(gridSize)
+    colorGrids()
+ })
+
+
+function getGridSize(gridSize) {
+for (i=0; i < gridSize; i++) {
     const container = document.querySelector('.container');
+    container.id = 'check'
     container.style.display = 'flex'
     container.style.flexWrap = 'wrap'
     container.style.maxWidth = '690px'
@@ -7,13 +29,21 @@ for (i=0; i <= 255; i++) {
     divGrid.classList.add('grid')
     divGrid.style.cssText = 'height: 40px; width: 40px; border: 1px solid black;' ;
     container.appendChild(divGrid); 
-}
-const divGrids = document.querySelectorAll('.grid')
+}}
 
+function colorGrids() {
+const divGrids = document.querySelectorAll('.grid')
 divGrids.forEach((grid) => {
     grid.addEventListener('mouseover', () => {
         grid.style.backgroundColor = 'green'
     })
+})}
 
-})
+
+
+
+
+
+
+
 
